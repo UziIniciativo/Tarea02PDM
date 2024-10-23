9ae52ef2c092445abffc0e88a3fa787a9e7fe0ad
UziIniciativo <104023096+UziIniciativo@users.noreply.github.com>
2024-10-22 21:35:06 -0600

    Agrega el menú derecho
    Iconos de escribir noticia, guardar notica y recargar noticias

--- app/src/main/java/com/example/tarea02/MainActivity.java
+++ app/src/main/java/com/example/tarea02/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.tarea02;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.*;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +16,35 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Formulario de selección de intereses
-        //setContentView(R.layout.activity_main);
-
-        // Pantalla de noticias
         setContentView(R.layout.news_activity);
+    }
+
+    //Inflar el menú
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options, menu);
+        return true;
+    }
+
+    // Manejar las opciones seleccionadas
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_cancel_order) {
+            Log.d("MenuAction", "Cancelar pedido");
+            Toast.makeText(this, "Noticias Guardadas", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_resend_order) {
+            Log.d("MenuAction", "Reenviar pedido");
+            Toast.makeText(this, "Recargar noticas", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_modify_order) {
+            Log.d("MenuAction", "Modificar pedido");
+            Toast.makeText(this, "Escribir notica", Toast.LENGTH_SHORT).show();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
