febe8a1dbcd34b74c4a63fda4ce8b6f9d2bf449b
UziIniciativo <104023096+UziIniciativo@users.noreply.github.com>
2024-10-23 23:01:01 -0600

    Agrega el menú drawer
    Menu derecho para seleccionar noticias por categoría

--- app/src/main/java/com/example/tarea02/MainActivity.java
+++ app/src/main/java/com/example/tarea02/MainActivity.java
@@ -5,18 +5,67 @@ import android.util.Log;
 import android.view.*;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_activity);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.abrir, R.string.cerrar);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_dinero) {
+            Toast.makeText(this, "Dinero seleccionado", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_deportes) {
+            Toast.makeText(this, "Deportes seleccionado", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_entretenimiento) {
+            Toast.makeText(this, "Entretenimiento seleccionado", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_salud) {
+            Toast.makeText(this, "Salud seleccionado", Toast.LENGTH_SHORT).show();
+        }
+
+        // Cerrar el Drawer después de la selección
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     //Inflar el menú
